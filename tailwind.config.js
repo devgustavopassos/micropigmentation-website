50daa18adb5ff72633ed92347019f3fcd5b6660c
Gustavo Passos <devgustavopassos@gmail.com>
2024-07-31 18:12:33 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+  theme: {
+    extend: {
+      colors: {
+        "gold-color": "#D6C272",
+        "green-color": "#12403C",
+      },
+    },
+  },
+  plugins: [],
+};
